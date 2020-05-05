82a175da37e8b079ef7b784b63214383c8f109c5
utpal-singh <utpal_singh@yahoo.com>
2020-05-05 17:29:25 +0530

    Variables block added

--- Main.go
+++ Main.go
@@ -2,12 +2,22 @@ package main
 
 import "fmt"
 
+var (
+	actorname string = "Utpal Singh"
+	companion string = "Male"
+)
+
+var (
+	age  int     = 21
+	year float64 = 3.75
+)
+
 func main() {
 	//i := 42.
 
 	//var j float32 = 42
 
-	var i int
-	i = 32.0
-	fmt.Printf("%v, %T", i, i)
+	// var i int
+	// i = 32.0
+	fmt.Printf("%v, %T", age, year)
 }
